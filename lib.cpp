741bfac651f2a3587919a95c48893e4aed4adb03
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-12-15 15:57:35 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
 
+#include "lib.h"
+bool funzione(char array2d[10][20], char array1d[20], int &c){
+    for (int i=0; i<10; i++){
+        for (int j=0; j<20; j++){
+            if (array2d [i][j] != array1d[j]) {
+                j=20;
+            }
+            else {
+                if (j==19){
+                    c=i;
+                }
+            }
+        }
+    }
+    return false;
+}
