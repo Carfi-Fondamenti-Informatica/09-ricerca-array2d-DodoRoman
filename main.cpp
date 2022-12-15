f0d1b94370c5cb0467ddc64103690d33a82fc20f
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-12-15 15:57:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    char array2d[10][20];
+    char array1d[20];
+    int c=0;
+    for (int i=0; i<10; i++){
+        for (int j=0; j<20; j++){
+            array1d[j] = ' ';
+            array2d[i][j] = ' ';
+        }
+    }
+    for (int i=0; i<10; i++){
+        cin >> array2d[i];
+    }
+    cin >> array1d;
+    if (funzione(array2d, array1d, c)==1){
+        cout << c << endl;
+    }
+    else{
+        cout << "non presente" << endl;
+    }
+    return 0;
 }
