45940f6a2ad7ceb45c8a7ae4ba795f6c6b6f61f9
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-12-15 16:10:21 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,14 +1,15 @@
 
 #include "lib.h"
-bool funzione(char array2d[10][20], char array1d[20], int &c){
-    for (int i=0; i<10; i++){
-        for (int j=0; j<20; j++){
-            if (array2d [i][j] != array1d[j]) {
+
+bool funzione(char array2d[10][20], char array1d[20],int &counter){
+    for(int i=0; i<10; i++){
+        for(int j=0; j<20; j++){
+            if(array2d[i][j]!=array1d[j]){
                 j=20;
-            }
-            else {
-                if (j==19){
-                    c=i;
+            }else{
+                if(j==19){
+                    counter=i;
+                    return true;
                 }
             }
         }
